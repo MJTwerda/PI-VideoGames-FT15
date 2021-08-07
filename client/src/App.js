deac81e0370c788e32d046a90affb9df8f4fde61
Mauri <dronm27a.z@gmail.com>
2021-08-07 00:42:47 -0300

    Boton inicio y /videogames funcando

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
+import React from "react";
 import './App.css';
+
+import { Route } from "react-router-dom";
+import Videogames from "./Components/Videogames.js";
+import Home from "./Components/Home.js";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Videogames</h1>
-      <h3>Ya me voy</h3>
-    </div>
+    <React.Fragment>
+      <Route exact path='/' component={Home} />
+      <Route path='/videogames' component={Videogames} />
+    </React.Fragment>
   );
 }
 
