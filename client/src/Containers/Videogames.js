96d8f7fd2f940c2b1165c6883c8386b87e877a77
Mauri <dronm27a.z@gmail.com>
2021-08-12 20:01:07 -0300

    Videogames funcionando

--- client/src/Containers/Videogames.js
+++ client/src/Containers/Videogames.js
@@ -1,30 +1,43 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { getAllGames, reset } from '../Actions/actions.js';
+import { getAllGames, reset, getGenres } from '../Actions/actions.js';
 
 import Game from '../Components/Game.js';
 import Pagination from '../Components/Pagination.js';
+import OrderGames from '../Components/OrderGames.js';
 
 export default function Videogames() {
 
-    const games = useSelector(state => state.searchGames)
     const [loading, setLoading] = useState(false);
+    const games = useSelector(state => state.searchGames);
+    const [seeGames, setSeeGames] = useState([]);
+
+    //Para el paginado
     const [currentPage, setcurrentPage] = useState(1);
     const [gamesPerPage, setgamesPerPage] = useState(9);
 
     const dispatch = useDispatch();
 
     useEffect(() => {
-        setLoading(true);
-        dispatch(reset());
-        dispatch(getAllGames());
-        setLoading(false);
-        console.log('ALL GAMESS: ', games)
+        let initial = async function() {
+            setLoading(true);
+            await dispatch(reset());
+            await dispatch(getAllGames());
+            await dispatch(getGenres()); 
+            setLoading(false);
+        }
+        initial()
+        console.log('ALL GAMESS: ', games);
+    }, []);
+
+    useEffect(() => {
+        setSeeGames(games);
+        console.log('SEE GAMES: copia del store: ', seeGames);
     }, []);
     
     const lastIndex = currentPage * gamesPerPage; //pag en la que estoy(2) * 9
     const firstIndex = lastIndex - gamesPerPage; //18 - 9
-    const currentGames = games.slice(firstIndex, lastIndex); //se lo mando a <Game />
+    const currentGames = games.slice(firstIndex, lastIndex) //se lo mando a <Game />
 
     //Funciones para paginar <Pagination />
     function paginate (pageNumber) {
@@ -36,10 +49,12 @@ export default function Videogames() {
     const paginateButtonNext  = ()  => {
         setcurrentPage(currentPage + 1)
     }
-
+    
     console.log('CURRENT GAMESSS: ', currentGames);
-     return (
+     
+    return (
          <div>
+            <OrderGames seeGames={seeGames}/>
             <Game games={currentGames} loading={loading} /> 
             <Pagination 
                 gamesPerPage={gamesPerPage} //9
